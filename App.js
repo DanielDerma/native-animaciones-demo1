11a8c596c743fab5ed631fb62bfd0b69423c5e7d
DanielDerma <danielderma94@gmail.com>
2022-03-01 11:48:01 -0700

    animation types

--- App.js
+++ App.js
@@ -1,25 +1,57 @@
 import { useState, useEffect, useRef } from "react";
-import { StyleSheet, Text, View, Animated } from "react-native";
+import { StyleSheet, Text, View, Animated, Easing } from "react-native";
 import { StatusBar } from "expo-status-bar";
 
 export default function App() {
-  const translation = useRef(new Animated.Value(0)).current;
+  const translation1 = useRef(new Animated.Value(0)).current;
+  const translation2 = useRef(new Animated.Value(0)).current;
+  const translation3 = useRef(new Animated.Value(0)).current;
 
   useEffect(() => {
-    Animated.timing(translation, {
-      toValue: 100,
-      duration: 1000,
-      useNativeDriver: true,
-    }).start();
+    Animated.stagger(125, [
+      Animated.timing(translation1, {
+        toValue: 1,
+        useNativeDriver: true,
+      }),
+      Animated.timing(translation2, {
+        toValue: 1,
+        useNativeDriver: true,
+      }),
+      Animated.timing(translation3, {
+        toValue: 1,
+        useNativeDriver: true,
+      }),
+    ]).start();
   }, []);
-
-  const styles = StyleSheet.create({
-    container: {
-      width: 100,
-      height: 100,
-      backgroundColor: "yellow",
-      transform: [{ translateX: translation }],
-    },
-  });
-  return <Animated.View style={styles.container} />;
+  return (
+    <>
+      <Animated.View
+        style={{
+          width: 100,
+          height: 100,
+          backgroundColor: "red",
+          marginBottom: 10,
+          opacity: translation1,
+        }}
+      />
+      <Animated.View
+        style={{
+          width: 100,
+          height: 100,
+          backgroundColor: "orange",
+          marginBottom: 10,
+          opacity: translation2,
+        }}
+      />
+      <Animated.View
+        style={{
+          width: 100,
+          height: 100,
+          backgroundColor: "blue",
+          marginBottom: 10,
+          opacity: translation3,
+        }}
+      />
+    </>
+  );
 }
